f03ce1f60933fa82cab01f6f369df8c50d61b88d
shijintao <710780244@qq.com>
2022-07-29 16:51:59 +0800

    个人信息模块

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,5 @@
 //每次调用ajax前会先调用此函数
-var _URL = 'http://127.0.0.1:3007'
+var _URL = 'http://127.0.0.1:8080'
 
 
 $.ajaxPrefilter(function (options) {
---
+++ assets/js/login.js
@@ -0,0 +1,32 @@
+$(function () {
+
+  //layui验证
+  var form = layui.form
+  var layer = layui.layer
+  form.verify({
+    pwd: [
+      /^[\S]{6,12}$/
+      , '密码必须6到12位，且不能出现空格'
+    ],
+    //校验两次密码必须一致
+    repwd: function (val) {
+      if (val != $('.reg-box [name=password]').val())
+        return '两次密码不一致'
+    }
+  });
+
+  //登录表单的提交事件
+  $('#form-login').on('submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/api/login',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) return layer.msg(res.message)
+        localStorage.setItem('token', res.token)
+        location.href = './index.html'
+      }
+    })
+  })
+})
